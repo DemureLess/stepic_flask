6655521153993f66d914aa7e09d11e5a255b3d53
Demureless <p.romanovskiy@gmail.com>
2020-01-02 14:03:34 +0300

    Полная переделка логики,

--- app.py
+++ app.py
@@ -1,72 +1,53 @@
-from flask import Flask, render_template, abort
-# from data import tours, title, subtitle, description, departures
 import json
 
+from flask import Flask, render_template
+
 with open("data.json", "r", encoding="utf-8") as f:
-     f = f.read()
-     data = json.loads(f)
+    f = f.read()
+    data = json.loads(f)
 
 meta = data["meta"]
 promo = data["promo"]
 departures = data["departures"]
 tours = data["tours"]
 
+for key in tours:
+    tours[key]['url'] = key  # используем id тура, в качестве url
+
 app = Flask(__name__)
+
+
+@app.context_processor
+def inject_departures():
+    return dict(departures=departures)
+
 
 @app.route('/')
 def main():
-    return render_template('index.html', promo=promo, departures=departures, tours=tours)
+    return render_template('index.html', promo=promo, tours=tours.values())
+
 
 @app.route('/from/<direction>')
 def from_direction(direction):
-
-    depart_meta = {}  # Результирующий словарь с метаданными
-    depart_tour = {}   # Отфильтрованный список туров
-    depart_price = []  # Для сортировки  цены
-    depart_nights = []  # для сортировки ночей
-
-    for x in tours.keys():
-        if tours[x]["departure"] == direction:
-            depart_tour[x] = tours[x]
-            depart_price.append(tours[x]['price'])
-            depart_nights.append(tours[x]['nights'])
-
-    depart_meta['depart_rus'] = departures.get(direction)
-    depart_meta['direction'] = direction
-
-    depart_meta['count'] = len(depart_tour)
-
-
-    depart_price.sort()  # Минимум . максимум цены
-    depart_meta['max_price'] = depart_price[-1]
-    depart_meta['min_price'] = depart_price[0]
-
-    depart_nights.sort()  # Минимум . максимум ночей
-    depart_meta['max_nights'] = depart_nights[-1]
-    depart_meta['min_nights'] = depart_nights[0]
-
-    print(depart_meta)
-
-    return render_template('direction.html', departures=departures, tours=depart_tour, meta=depart_meta)
+    depart_tour = {}  # Отфильтрованный список туров
+    for tour_id in tours.keys():
+        if tours[tour_id]["departure"] == direction:
+            depart_tour[tour_id] = tours[tour_id]
+    return render_template('direction.html', tours=depart_tour.values(), direction=direction)
 
 
 @app.route('/tours/<id>')
 def toursid(id):
-    depart_meta = {}
     tour = tours.get(id)
-    depart = tour.get('departure')
-    depart_meta['depart_rus'] = departures.get(depart)
-    depart_meta['direction'] = depart
-
-    print(depart)
-    print(depart_meta)
-
-    if tour == None:
-        abort(404)
+    if tour:
+        return render_template('tour.html', tours=tour)
     else:
-        return render_template('tour.html', tours=tour, departures=departures, meta=depart_meta)
+        return page_not_found(404)
 
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    app.run()
